9d70edf85391d360441ec974e627986f4a23ec05
Diamond <2210054954@qq.com>
2025-02-21 10:43:45 +0800

    利用阿里云OSS存储实现文件上传

--- sky-common/src/main/java/com/sky/properties/AliOssProperties.java
+++ sky-common/src/main/java/com/sky/properties/AliOssProperties.java
@@ -4,6 +4,9 @@ import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
+/**
+ * @author Diamond
+ */
 @Component
 @ConfigurationProperties(prefix = "sky.alioss")
 @Data
@@ -13,5 +16,4 @@ public class AliOssProperties {
     private String accessKeyId;
     private String accessKeySecret;
     private String bucketName;
-
 }
---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,50 @@
+package com.sky.controller.admin;
+
+import com.sky.constant.MessageConstant;
+import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.UUID;
+
+/**
+ * @Author Java小猪
+ * @Date 2025/2/20 16:17
+ */
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用接口")
+@Slf4j
+public class CommonController {
+
+    @Resource
+    private AliOssUtil aliOssUtil;
+
+    @PostMapping("/upload")
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file) {
+        log.info("文件上传：{}", file);
+        try {
+            // 原始文件名
+            String originalFilename = file.getOriginalFilename();
+            // 截取原始文件名的后缀
+            String extion = originalFilename.substring(originalFilename.lastIndexOf("."));
+            // 构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extion;
+            // 文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
+        } catch (IOException e) {
+            log.error("文件上传失败：{}", e);
+        }
+        return Result.error(MessageConstant.UPLOAD_FAILED);
+    }
+}
