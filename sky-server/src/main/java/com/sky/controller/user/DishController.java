7c230795b8417a95fd4c9ee24ec1298c75192015
Diamond <2210054954@qq.com>
2025-03-03 17:08:44 +0800

    用户端根据分类id查询菜品业务代码优化

--- sky-server/src/main/java/com/sky/controller/user/DishController.java
+++ sky-server/src/main/java/com/sky/controller/user/DishController.java
@@ -1,8 +1,10 @@
 package com.sky.controller.user;
 
+import com.sky.constant.StatusConstant;
 import com.sky.entity.Dish;
 import com.sky.result.Result;
 import com.sky.service.DishService;
+import com.sky.vo.DishVO;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,8 +33,14 @@ public class DishController {
      */
     @GetMapping("/list")
     @ApiOperation("根据分类id查询菜品")
-    public Result<List<Dish>> list(Long categoryId){
-        List<Dish> list = dishService.list(categoryId);
+    public Result<List<DishVO>> list(Long categoryId) {
+        Dish dish = new Dish();
+        dish.setCategoryId(categoryId);
+        //查询起售中的菜品
+        dish.setStatus(StatusConstant.ENABLE);
+
+        List<DishVO> list = dishService.listWithFlavor(dish);
+
         return Result.success(list);
     }
 }
